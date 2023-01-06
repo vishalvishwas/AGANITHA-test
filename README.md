64ccae7305920dc970065315ad573bf675231862
vishalvishwas <54342731+vishalvishwas@users.noreply.github.com>
2023-01-06 13:57:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AGANITHA-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-elnatk)
---
+++ index.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+  console.log("amazon");
+  } else if (i % 3 === 0) {
+  console.log("google");
+  } else if (i % 5 === 0) {
+  console.log("facebook");
+  } else {
+  console.log(i);
+  }
+  }
